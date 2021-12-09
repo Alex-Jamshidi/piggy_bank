cfe7b6dc44d285638b67090b8bc6f4426c2a1bd3
Alexander Jamshidi <alexanderjamshidi@Alexanders-Air.localdomain>
2021-12-09 13:01:20 +0000

    updated to workshop version of files

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,1 +1,13 @@
-puts "coins"
+class PiggyBank
+    def initialize
+        @has_coin = false
+    end
+
+    def deposit_coin(coin)
+        @has_coin = true
+    end
+
+    def shake
+        return "Cling!" if @has_coin
+    end
+end
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -1,2 +1,36 @@
-describe user do
+require 'piggy_bank'
+
+describe PiggyBank do
+    it "can receive coins" do
+        # Arrange
+        piggy_bank = PiggyBank.new
+        
+        # Act, Assert
+        #1 respond_to
+        #2 to_not raise_error
+        expect { piggy_bank.deposit_coin("50p") }.to_not raise_error
+    end
+
+    it "clings when we shake it and there are coins in it" do
+        # Arrange
+        piggy_bank = PiggyBank.new
+        piggy_bank.deposit_coin "20p"
+
+        # Act
+        result = piggy_bank.shake
+
+        # Assert
+        expect(result).to eq("Cling!")
+    end
+
+    it "does not cling when we shake it and there are no coins in it" do
+        # Arrange
+        piggy_bank = PiggyBank.new
+
+        # Act
+        result = piggy_bank.shake
+
+        # Assert
+        expect(result).not_to eq("Cling!")
+    end
 end
